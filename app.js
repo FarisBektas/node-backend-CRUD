3a24c413b7df39350717f045ebdcd993aaf38ea3
bektafa <bektafa@users.noreply.github.com>
2021-05-31 17:55:12 +0200

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const app = express();
-const databaseString = process.env.DB_CONNECTION_STRING;
+const databaseString = process.env.DB_CONNECTION_STRING || "mongodb+srv://asdf2363:risfakralj1@cluster0.iaekf.mongodb.net/SE-project?retryWrites=true&w=majority";
 
 mongoose.connect(databaseString,{
     useNewUrlParser: true,
